3d442553bf63ad37ed47316161bd056f8bf36dee
David Guttman <david@davidguttman.com>
2014-02-21 16:25:48 -0800

    test tweak for phantomjs

--- test/index.js
+++ test/index.js
@@ -14,17 +14,27 @@ var columns =
   ]
 
 var ht = hypnotable(columns)
-data.forEach(ht.write.bind(ht))
+data.forEach(function(item) { ht.write(item) })
 
 window.document.body.appendChild(ht.el)
 
 test('table should sort', function(t) {
   var cell1 = ht.el.querySelector('tbody tr td')
-  t.assert(cell1.innerHTML === 'tb')
+  t.ok(cell1.innerHTML === 'tb')
 
-  ht.el.querySelector('th').click()
+  eventFire(ht.el.querySelector('th'), 'click')
+ 
   cell1 = ht.el.querySelector('tbody tr td')
-  t.assert(cell1.innerHTML === 'ta')
+  t.ok(cell1.innerHTML === 'ta')
   t.end()
 })
   
+function eventFire(el, etype){
+  if (el.fireEvent) {
+    (el.fireEvent('on' + etype));
+  } else {
+    var evObj = document.createEvent('Events');
+    evObj.initEvent(etype, true, false);
+    el.dispatchEvent(evObj);
+  }
+}
